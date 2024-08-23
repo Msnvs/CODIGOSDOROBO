f6745b268f07b8fb326048cc8fe299cbde470d18
Msnvs <103458670+Msnvs@users.noreply.github.com>
2024-08-23 02:21:02 -0300

    Update Sensor com cancela Servo.h

--- int sensor = A0; float Temperatura;  void setup() {   Serial.begin(9600);  }  void loop() {   int valor = analogRead(sensor); Temperatura = ((valor*5)/1023)/Sensor com cancela Servo.h
+++ int sensor = A0; float Temperatura;  void setup() {   Serial.begin(9600);  }  void loop() {   int valor = analogRead(sensor); Temperatura = ((valor*5)/1023)/Sensor com cancela Servo.h
@@ -1,33 +1,34 @@
+
 #include <Servo.h>
 
-const int sensorPin = A0; // Sensor de linha conectado ao pino A0
-const int servoPin = 9; // Servo motor conectado ao pino 9
-const int gateOpenAngle = 120; // Ângulo de abertura da cancela
-const int gateCloseAngle = 30; // Ângulo de fechamento da cancela
+const int Pinosensor = A0; 
+const int Pinocentral = 9;
+const int PortaoAbre = 120;
+const int POrtaoFecha = 30;
 
-Servo gateServo; // Cria um objeto Servo para controlar o servo motor
+Servo Portao; 
 
 void setup() {
-  gateServo.attach(servoPin); // Inicializa o servo motor no pino 9
-  pinMode(sensorPin, INPUT); // Define o pino do sensor de linha como entrada
+  Portao.attach(Pinocentral); 
+  pinMode(Pinosensor, INPUT); 
 }
 
 void loop() {
-  int sensorValue = analogRead(sensorPin); // Lê o valor do sensor de linha
+  int ArivarCensor = analogRead(Pinosensor); 
 
-  if (sensorValue < 500) { // Se o sensor de linha não detectar a linha
-    openGate(); // Abre a cancela
-  } else { // Se o sensor de linha detectar a linha
-    closeGate(); // Fecha a cancela
+  if (ArivarCensor < 500) { 
+    Abrir();
+  } else { 
+    Fechar(); 
   }
 
-  delay(50); // Aguarda 50 ms antes de ler o sensor novamente
+  delay(50); 
 }
 
-void openGate() {
-  gateServo.write(gateOpenAngle); // Abre a cancela
+void Abrir() {
+  Portao.write(PortaoAbre);
 }
 
-void closeGate() {
-  gateServo.write(gateCloseAngle); // Fecha a cancela
+void Fechar() {
+  Portao.write(POrtaoFecha);
 }
